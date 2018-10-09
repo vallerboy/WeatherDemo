c5314f0114c96f3dc23c9ab9e43f009708d60f9b
Oskix <oskar.polak@akademiakodu.pl>
2018-10-09 20:26:55 +0200

    ad

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -5,6 +5,8 @@ import com.example.demo.models.WeatherModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
@@ -12,7 +14,12 @@ public class MainController {
 
     @GetMapping("/")
     public String index(Model model) {
-        String link   = "http://api.openweathermap.org/data/2.5/weather?q=Krakow&appid=ef2028e98b54649bf1f4c4582631f350";
+        return "index";
+    }
+
+    @PostMapping("/")
+    public  String index(@RequestParam("city") String city, Model model){
+        String link   = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ef2028e98b54649bf1f4c4582631f350";
 
         RestTemplate restTemplate = new RestTemplate();
         WeatherModel weatherModel = restTemplate.getForObject(link, WeatherModel.class);
