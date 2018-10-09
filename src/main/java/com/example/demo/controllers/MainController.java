3a5b4b03d9da2a352a674599ca244bdb93dbc41f
Oskix <oskar.polak@akademiakodu.pl>
2018-10-09 19:56:16 +0200

    ad

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -9,6 +9,8 @@ public class MainController {
 
     @GetMapping("/")
     public String index() {
+        String link = "http://api.openweathermap.org/data/2.5/weather?q=Krakow&appid=ef2028e98b54649bf1f4c4582631f350"
+
 
 
     }
